a7487094de9c0e136cb881007cbf50187e453a11
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-10-22 17:08:18 +0100

    colliders working.

--- EvoFish/EvoFish/Assets/Scripts/BirdBehaviour.cs
+++ EvoFish/EvoFish/Assets/Scripts/BirdBehaviour.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BirdBehaviour : MonoBehaviour {
-
-    private Vector3 _birdMovement = new Vector3(-1.0f, 0.0f, 0.0f);
+    
     private float _cycleTimer = 0;
     private bool _detected = false;
     private float _diffDisty = 0;
@@ -20,7 +19,7 @@ public class BirdBehaviour : MonoBehaviour {
 	void Update () {
         // Get difference in difference between attached object and the player (x axis)
         float _diffDistx = gameObject.transform.position.x - GameObject.Find("Player").transform.position.x;
-        print(_cycleTimer);
+
         if (_diffDistx <= 5.0f) {
             _detected = true;
         }
--- EvoFish/EvoFish/Assets/Scripts/CatBehaviour.cs
+++ EvoFish/EvoFish/Assets/Scripts/CatBehaviour.cs
@@ -2,25 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatBehaviour : MonoBehaviour {
+public class CatBehaviour : MonoBehaviour
+{
 
-    Vector3 _catMovement = new Vector3(-1.0f, 0.0f, 0.0f);
+    private float _moveSpeed = 1.3f;
+    private bool _detected = false;
+    private float _frequency = 1.02f;  // Speed of sine movement
+    private float _magnitude = 0;   // Size of sine movement
+    private Vector3 _axis;
+    private float _diffDistX = 0;
+    private Vector3 _pos;
+    private bool catJumped = false;
+    private float _cycleTimer = 0;
+    private float oriY = 0;
 
     // Use this for initialization
     void Start()
     {
-
+        _pos = transform.position;
+        _axis = new Vector3(-1.0f, 0.0f, 0.0f);
+        oriY = _pos.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get difference in difference between attached object and the player (x axis)
-        float _diffDist = Mathf.Abs(GameObject.Find("Player").transform.position.x - gameObject.transform.position.x);
-        if (_diffDist <= 2.0f)
+        _diffDistX = gameObject.transform.position.x - GameObject.Find("Player").transform.position.x;
+        if (!catJumped && _diffDistX <= 5.0f)
         {
-            // Cat detects the player
-            transform.Translate(_catMovement * Time.deltaTime);
+            _detected = true;
+            _magnitude = Mathf.Abs(GameObject.Find("Player").transform.position.x - gameObject.transform.position.x);
+            catJumped = true;
+        }
+
+        if (_detected)
+        {
+            _cycleTimer += Time.deltaTime;
+            _pos.y -= (oriY * Time.deltaTime * _moveSpeed);
+            //_pos.y -= (1.0f * Time.deltaTime * _moveSpeed);
+            transform.position = _pos + _axis * Mathf.Sin((Mathf.PI/2.0f) * _cycleTimer * _frequency) * _magnitude;
         }
     }
 }
