8b278ac1b7ca60611d5560c202571863a568f5b0
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-10-22 13:43:26 +0100

    Collision detection and object tagging.

---
+++ EvoFish/EvoFish/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+
+    Vector3 vector = new Vector3(1.0f, 0.0f, 0.0f);
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(vector * Time.deltaTime);
+
+        // Debugging purposes. Had to check multiple collisions.
+        if (Input.GetKeyDown("space"))
+        {
+            vector *= -1;
+        }
+    }
+}
