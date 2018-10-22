8e0e098f1124adfdfe7d2535ddaf9455edda6cbc
Tim-Surface <40219124@live.napier.ac.uk>
2018-10-22 13:24:47 +0100

    Ground block that prevents the player falling below the ground level. Camera positioned to follow player.

---
+++ EvoFish/EvoFish/Assets/GroundBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundBlock : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    private Vector3 baseLevel = new Vector3(0.0f, 0.0f, 0.0f);
+    // Use this for initialization
+    void Start()
+    {
+        baseLevel.y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = baseLevel + new Vector3(player.position.x, 0, 0);
+    }
+}
