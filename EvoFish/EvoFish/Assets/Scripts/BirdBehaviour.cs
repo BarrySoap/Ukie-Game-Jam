3e683bdae2be4addd1990ffa433febc32d9222ce
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-10-22 15:38:56 +0100

    Consistency between variable names.

--- EvoFish/EvoFish/Assets/Scripts/BirdBehaviour.cs
+++ EvoFish/EvoFish/Assets/Scripts/BirdBehaviour.cs
@@ -6,14 +6,14 @@ public class BirdBehaviour : MonoBehaviour {
 
     private Vector3 _birdMovement = new Vector3(-1.0f, 0.0f, 0.0f);
     private float _cycleTimer = 0;
-    private bool detected = false;
+    private bool _detected = false;
     private float _diffDisty = 0;
-    private float pos = 0;
+    private float _pos = 0;
 
     // Use this for initialization
     void Start () {
         _diffDisty = Mathf.Abs(GameObject.Find("Player").transform.position.y - gameObject.transform.position.y);
-        pos = gameObject.transform.position.y;
+        _pos = gameObject.transform.position.y;
     }
 	
 	// Update is called once per frame
@@ -22,18 +22,18 @@ public class BirdBehaviour : MonoBehaviour {
         float _diffDistx = gameObject.transform.position.x - GameObject.Find("Player").transform.position.x;
         print(_cycleTimer);
         if (_diffDistx <= 5.0f) {
-            detected = true;
+            _detected = true;
         }
         if (_cycleTimer > 8.0f) {
-            detected = false;
+            _detected = false;
         }
-        if (detected) {
+        if (_detected) {
             _cycleTimer += Time.deltaTime;
             transform.Translate(-1.0f * Time.deltaTime, 0.0f, 0.0f);
             Vector3 swoop = transform.position;
-            swoop.y = pos + ((Mathf.Cos(_cycleTimer * Mathf.PI / 4.0f) - 1) / 2) * _diffDisty;
+            swoop.y = _pos + ((Mathf.Cos(_cycleTimer * Mathf.PI / 4.0f) - 1) / 2) * _diffDisty;
             transform.position = swoop;
-            detected = false;
+            _detected = false;
         }
     }
 }
