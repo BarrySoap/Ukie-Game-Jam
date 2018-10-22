317b6098c505c0966ef7e92022f5f172dde0cf4c
Dan's Laptop <dkorsah15@aol.co.uk>
2018-10-22 19:00:35 +0100

    End zone basics

---
+++ EvoFish/EvoFish/Assets/Scripts/EndZone.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndZone : MonoBehaviour
+{
+
+	public float Countdown;
+	public string SceneToLoad;
+
+	private float activationTimer;
+	private bool activated = false;
+
+	private BoxCollider2D endCollider;
+
+	// Use this for initialization
+	void Start ()
+	{
+		endCollider = gameObject.GetComponent<BoxCollider2D> ();
+		activationTimer = Countdown -= Time.deltaTime;
+
+		if (SceneToLoad == null)
+			SceneToLoad = "Dan";
+	}
+
+	//get activated by collision
+	public void Activate ()
+	{
+		Debug.Log ("Activate");
+		activated = true;
+	}
+
+	void Update ()
+	{
+		//decrement the timer if activated.
+		if (activated)
+		{
+			activationTimer -= Time.deltaTime;
+
+			//do stuff during the countdown time
+			//...
+		}
+
+		//if timer runs out 
+		if (activationTimer <= 0)
+		{
+			SceneManager.LoadScene (SceneToLoad);
+		}
+	}
+}
