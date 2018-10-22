9e3c2a78f52ca153d4424c618fadded047763297
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-10-22 14:56:51 +0100

    Birds move in a cos wave after detecting the player.

--- EvoFish/EvoFish/Assets/Scripts/BirdBehaviour.cs
+++ EvoFish/EvoFish/Assets/Scripts/BirdBehaviour.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class BirdBehaviour : MonoBehaviour {
 
     Vector3 _birdMovement = new Vector3(-1.0f, 0.0f, 0.0f);
+    float timer = 0;
+    bool detected = false;
+    float _diffDisty = 0;
+    float pos = 0;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        _diffDisty = Mathf.Abs(GameObject.Find("Player").transform.position.y - gameObject.transform.position.y);
+        pos = gameObject.transform.position.y;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(_birdMovement * Time.deltaTime);
+        // Get difference in difference between attached object and the player (x axis)
+        float _diffDistx = Mathf.Abs(GameObject.Find("Player").transform.position.x - gameObject.transform.position.x);
+        if (_diffDistx <= 5.0f || _diffDistx >= 5.0f)
+        {
+            timer += Time.deltaTime;
+            // Cat detects the player
+            transform.Translate(-1.0f * Time.deltaTime, 0.0f, 0.0f);
+            Vector3 swoop = transform.position;
+            swoop.y = pos + ((Mathf.Cos(timer * Mathf.PI / 4.0f) - 1) / 2) * _diffDisty;
+            transform.position = swoop;
+            print(timer);
+        }
     }
 }
---
+++ EvoFish/EvoFish/Assets/Scripts/CatBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatBehaviour : MonoBehaviour {
+
+    Vector3 _catMovement = new Vector3(-1.0f, 0.0f, 0.0f);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get difference in difference between attached object and the player (x axis)
+        float _diffDist = Mathf.Abs(GameObject.Find("Player").transform.position.x - gameObject.transform.position.x);
+        if (_diffDist <= 2.0f)
+        {
+            // Cat detects the player
+            transform.Translate(_catMovement * Time.deltaTime);
+        }
+    }
+}
