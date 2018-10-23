c49da660e94152db532eddd53388bc8cbbc03480
Dan's Laptop <dkorsah15@aol.co.uk>
2018-10-23 10:25:30 +0100

    Set ability allowance and animator level based on lavel progress.

--- EvoFish/EvoFish/Assets/Scripts/PlayerController.cs
+++ EvoFish/EvoFish/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -29,50 +30,58 @@ public class PlayerController : MonoBehaviour
 	private float holdJumpTimer = 0;
 	private float defaultSize;
 	private Vector2 defaultOrigin;
+	private int evo;
 
 	// Use this for initialization
-	void Start()
+	void Start ()
 	{
-		playerBody = gameObject.GetComponent<Rigidbody2D>();
-		playerCollider = gameObject.GetComponent<BoxCollider2D>();
+		playerBody = gameObject.GetComponent<Rigidbody2D> ();
+		playerCollider = gameObject.GetComponent<BoxCollider2D> ();
 		defaultSize = playerCollider.size.y;
 		defaultOrigin = playerCollider.offset;
+
+		LevelCheck ();
+		anim.SetInteger ("level", evo - 1);
 		//Debug.Log(defaultSize + ", " + defaultOrigin);
 
 	}
 
 	// Update is called once per frame
-	void Update()
+	void Update ()
 	{
 		//constant movement
-		MoveRight();
+		MoveRight ();
 
 		//jump logic
-		JumpInputs();
+		JumpInputs ();
 		holdJumpTimer -= Time.deltaTime;
 
-		//dash logic
-		DashInputs();
-		dashTimer -= Time.deltaTime;
-
 		//Duck logic
-		DuckInputs();
+		if (evo >= 3) //check correct level
+			DuckInputs ();
+
+		//dash logic
+		if (evo >= 4) //check correct level
+			DashInputs ();
+
+		//decrement dash cooldown
+		dashTimer -= Time.deltaTime;
 
 	}
 
 	//set player to grounded to allow them to jump when colliding with floor
-	private void OnCollisionEnter2D(Collision2D col)
+	private void OnCollisionEnter2D (Collision2D col)
 	{
 		//Debug.Log("Collided with " + col.gameObject.name);
 		if (col.gameObject.tag == "Platform")
 		{
-			anim.SetBool("jump", false);
+			anim.SetBool ("jump", false);
 			isGrounded = true;
 		}
 	}
 
 	//when character is jumping it will exit collision and no longer be grounded
-	private void OnCollisionExit2D(Collision2D col)
+	private void OnCollisionExit2D (Collision2D col)
 	{
 		//Debug.Log("Jumping");
 
@@ -85,92 +94,119 @@ public class PlayerController : MonoBehaviour
 	}
 
 	//take jump inputs
-	void JumpInputs()
+	void JumpInputs ()
 	{
-		if (Input.GetAxis("Jump") > 0)
+		if (Input.GetAxis ("Jump") > 0)
 		{
-			anim.SetBool("jump", true);
+			anim.SetBool ("jump", true);
 			if (isGrounded)
 			{
-				Jump();
+				Jump ();
 			}
 			else
-				HoldJump();
+			{
+				//check hold jump is unlocked
+				if (evo >= 2)
+					HoldJump ();
+			}
 		}
 	}
 
 	//physical jump
-	void Jump()
+	void Jump ()
 	{
-		playerBody.velocity = new Vector2(playerBody.velocity.x, jumpForce);
+		playerBody.velocity = new Vector2 (playerBody.velocity.x, jumpForce);
 	}
 
-	void HoldJump()
+	void HoldJump ()
 	{
 		if (holdJumpTimer > 0)
-			playerBody.velocity += new Vector2(playerBody.velocity.x, holdMultiplier * jumpForce) * Time.deltaTime;
+			playerBody.velocity += new Vector2 (playerBody.velocity.x, holdMultiplier * jumpForce) * Time.deltaTime;
 	}
 
-	void DashInputs()
+	void DashInputs ()
 	{
 		//if dash cooled down allow another dash
 		if (dashTimer <= 0)
 		{
-			if (Input.GetAxis("Fire1") > 0)
+			if (Input.GetAxis ("Fire1") > 0)
 			{
-				StartCoroutine(Dash());
+				StartCoroutine (Dash ());
 				dashTimer = dashCooldown;
 			}
 		}
 	}
 
 	//coroutine to do dash actions over a period of time
-	IEnumerator Dash()
+	IEnumerator Dash ()
 	{
-		anim.SetBool("dash", true);
+		anim.SetBool ("dash", true);
 		runSpeed = runSpeed * dashMultiplier;
-		yield return new WaitForSeconds(dashDuration);
-		anim.SetBool("dash", false);
+		yield return new WaitForSeconds (dashDuration);
+		anim.SetBool ("dash", false);
 		runSpeed = runSpeed / dashMultiplier;
 	}
 
-	void DuckInputs()
+	void DuckInputs ()
 	{
-		if (Input.GetAxis("Vertical") < 0)
+		if (Input.GetAxis ("Vertical") < 0)
 		{
-			anim.SetBool("duck", true);
-			Duck(true);
+			anim.SetBool ("duck", true);
+			Duck (true);
 		}
 		else
 		{
-			anim.SetBool("duck", false);
-			Duck(false);
+			anim.SetBool ("duck", false);
+			Duck (false);
 		}
 	}
 
-	void Duck(bool ducking)
+	void Duck (bool ducking)
 	{
 		if (ducking)
 		{
 			//shrink and lower hitbox for duck action
-			playerCollider.size = new Vector2(playerCollider.size.x, defaultSize / 2);
-			playerCollider.offset = new Vector2(0, defaultOrigin.y - (defaultSize / 5));
+			playerCollider.size = new Vector2 (playerCollider.size.x, defaultSize / 2);
+			playerCollider.offset = new Vector2 (0, defaultOrigin.y - (defaultSize / 5));
 		}
 		else
 		{
 			//restet hitbox to default values
-			playerCollider.size = playerCollider.size = new Vector2(playerCollider.size.x, defaultSize);
+			playerCollider.size = playerCollider.size = new Vector2 (playerCollider.size.x, defaultSize);
 			playerCollider.offset = defaultOrigin;
 		}
 
 	}
 
 	//constantly moving
-	void MoveRight()
+	void MoveRight ()
 	{
 		if (moving == true)
-			playerBody.velocity = new Vector2(runSpeed, playerBody.velocity.y);
+			playerBody.velocity = new Vector2 (runSpeed, playerBody.velocity.y);
 		else
-			playerBody.velocity = new Vector2(0, playerBody.velocity.y);
+			playerBody.velocity = new Vector2 (0, playerBody.velocity.y);
+	}
+
+	//set allowed abilities
+	void LevelCheck ()
+	{
+		switch (SceneManager.GetActiveScene ().name)
+		{
+			case "Level1":
+				evo = 1;
+				break;
+			case "Level2":
+				evo = 2;
+				break;
+			case "Level3":
+				evo = 3;
+				break;
+			case "Level4":
+				evo = 4;
+				break;
+			default:
+				evo = 0;
+				break;
+		}
 	}
 }
--- EvoFish/EvoFish/Assets/Scripts/WorldInteraction.cs
+++ EvoFish/EvoFish/Assets/Scripts/WorldInteraction.cs
@@ -6,33 +6,33 @@ public class WorldInteraction : MonoBehaviour
 {
 	BoxCollider2D playerCollider;
 
-	private void Start()
+	private void Start ()
 	{
-		playerCollider = gameObject.GetComponent<BoxCollider2D>();
+		playerCollider = gameObject.GetComponent<BoxCollider2D> ();
 	}
 
-	private void OnTriggerEnter2D(Collider2D col)
+	private void OnTriggerEnter2D (Collider2D col)
 	{
 		//end of level interaction
 		if (col.gameObject.tag == "End")
 		{
-			Debug.Log("Send Message");
+			Debug.Log ("Send Message");
 			//col.gameObject.SendMessage("Activate");
-			EndZone e = col.gameObject.GetComponent<EndZone>();
-			e.Activate();
+			EndZone e = col.gameObject.GetComponent<EndZone> ();
+			e.Activate ();
 		}
 
 		//hit by enemy/obstacle
 		if (col.gameObject.tag == "Obstacle")
 		{
-			Debug.Log("Obstacle encountered!");
+			Debug.Log ("Obstacle encountered!");
 
-			EndZone e = col.gameObject.GetComponent<EndZone>();
-			e.Activate();
+			EndZone e = col.gameObject.GetComponent<EndZone> ();
+			e.Activate ();
 
 			//simulate death animation
 			playerCollider.enabled = false;
-			gameObject.GetComponent<PlayerController>().moving = false;
+			gameObject.GetComponent<PlayerController> ().moving = false;
 		}
 	}
 }
