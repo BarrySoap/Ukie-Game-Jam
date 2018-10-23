685f322d7bb467c47ba7404973796e1d377af3e8
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-10-23 13:17:19 +0100

    Infinite platforms.

--- EvoFish/EvoFish/Assets/Scripts/MoveGround.cs
+++ EvoFish/EvoFish/Assets/Scripts/MoveGround.cs
@@ -5,27 +5,25 @@ using UnityEngine;
 public class MoveGround : MonoBehaviour
 {
     [SerializeField]
-    Transform otherBackground;
+    Transform otherPlatform;
     [SerializeField]
     Transform camera;
-
-    public float temp;
 
     // Use this for initialization
     void Start()
     {
-        temp = camera.position.x;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.position.x + 9.085f == (Mathf.Round(camera.position.x) - 7.8f) && otherBackground.position.x < camera.position.x)
+        if (gameObject.transform.position.x == Mathf.Round(camera.position.x - 8) && otherPlatform.position.x < camera.position.x)
         {
             // Move the platform by double the width of a platform on the x axis. (hacky lol)
-            gameObject.transform.Translate(new Vector3(22.7125f, 0.0f, 0.0f));
+            otherPlatform.Translate(new Vector3(36.0f, 0.0f, 0.0f));
         }
-        if (otherBackground.position.x == Mathf.Round(camera.position.x - 8) && gameObject.transform.position.x < camera.position.x)
+        if (otherPlatform.position.x == Mathf.Round(camera.position.x - 8) && gameObject.transform.position.x < camera.position.x)
         {
             gameObject.transform.Translate(new Vector3(36.0f, 0.0f, 0.0f));
         }
