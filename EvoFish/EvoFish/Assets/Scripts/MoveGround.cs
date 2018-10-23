8f2a278dd85a201de3091833f9086a4e5bb1bca8
40206111 <40206111@live.napier.ac.uk>
2018-10-23 13:41:54 +0100

    The Floor wtih Sprites

--- EvoFish/EvoFish/Assets/Scripts/MoveGround.cs
+++ EvoFish/EvoFish/Assets/Scripts/MoveGround.cs
@@ -18,12 +18,15 @@ public class MoveGround : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.position.x == Mathf.Round(camera.position.x - 8) && otherPlatform.position.x < camera.position.x)
+        Debug.Log("spam");
+        if (Mathf.Round(gameObject.transform.position.x) == Mathf.Round(camera.position.x) &&
+            otherPlatform.position.x < camera.position.x)
         {
             // Move the platform by double the width of a platform on the x axis. (hacky lol)
             otherPlatform.Translate(new Vector3(36.0f, 0.0f, 0.0f));
         }
-        if (otherPlatform.position.x == Mathf.Round(camera.position.x - 8) && gameObject.transform.position.x < camera.position.x)
+        if (Mathf.Round(otherPlatform.position.x) == Mathf.Round(camera.position.x) &&
+            gameObject.transform.position.x < camera.position.x)
         {
             gameObject.transform.Translate(new Vector3(36.0f, 0.0f, 0.0f));
         }
