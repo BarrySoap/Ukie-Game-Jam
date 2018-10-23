3633b6bfde5863d2631afdb01e1ee221d7b1411e
40206111 <40206111@live.napier.ac.uk>
2018-10-23 09:53:29 +0100

    Mole -Flips

--- EvoFish/EvoFish/Assets/Scripts/MoleBehaviour.cs
+++ EvoFish/EvoFish/Assets/Scripts/MoleBehaviour.cs
@@ -9,6 +9,7 @@ public class MoleBehaviour : MonoBehaviour
     [SerializeField]
     private Animator anim;
 
+    private float _diffDistx = 0;
     private bool _detected = false;
 
     // Use this for initialization
@@ -20,7 +21,7 @@ public class MoleBehaviour : MonoBehaviour
     void Update()
     {
         // Get difference in difference between attached object and the player (x axis)
-        float _diffDistx = gameObject.transform.position.x - GameObject.Find("Player").transform.position.x;
+       _diffDistx = gameObject.transform.position.x - GameObject.Find("Player").transform.position.x;
 
         if (_diffDistx <= 5.0f)
         {
@@ -35,6 +36,15 @@ public class MoleBehaviour : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        anim.SetBool("attack", true);
+        if (col.gameObject.tag == "Player")
+        {
+            if (_diffDistx < 0.0f)
+            {
+                Vector3 temp = gameObject.transform.localScale;
+                temp.x *= -1;
+                gameObject.transform.localScale = temp;
+            }
+            anim.SetBool("attack", true);
+        }
     }
 }
